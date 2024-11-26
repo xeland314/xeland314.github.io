fd5154e9c52a004af565c5377880fae6d394d52f
Christopher Villamarín <alexpila.314@gmail.com>
2024-11-25 22:45:17 -0500

    fix card footer grid and buttons

--- src/app/sections/projects/components/cardFooter.tsx
+++ src/app/sections/projects/components/cardFooter.tsx
@@ -7,13 +7,13 @@ interface ICardFooterProps {
 
 export function CardFooter({ githubLink, demoLink }: ICardFooterProps) {
   return (
-    <div className="flex items-center max-sm:flex-col justify-between gap-2 mt-4">
+    <div className="grid grid-cols-1 xs:grid-cols-2 items-center justify-between gap-2 mt-4">
       {githubLink && (
         <a
           href={githubLink}
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center bg-gray-500 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded"
+          className="flex flex-row items-center justify-center bg-gray-500 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded"
         >
           <span className="mr-2">Repositorio</span>
           <Github />
@@ -24,7 +24,7 @@ export function CardFooter({ githubLink, demoLink }: ICardFooterProps) {
           href={demoLink}
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center bg-green-600 hover:bg-green-700 text-white font-medium py-2 px-4 rounded"
+          className="flex flex-row items-center justify-center bg-green-600 hover:bg-green-700 text-white font-medium py-2 px-4 rounded"
         >
           <span className="mr-2">Demo</span>
           <OpenLink />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        xs: "330px", // Define tu media query personalizada
+      },
     },
   },
   plugins: [],
