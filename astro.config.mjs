3d4345e994431c83264777345f8aa59de55ad7b8
Christopher Villamarín <alexpila.314@gmail.com>
2025-08-01 17:12:14 -0500

    feat: enhance sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,34 @@ import db from "@astrojs/db";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
+const MY_SITE = "https://xeland314.github.io";
+
 // https://astro.build/config
 export default defineConfig({
+  site: MY_SITE,
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [db(), react()],
+  integrations: [
+    db(),
+    react(),
+    sitemap({
+      customPages: [
+        `${MY_SITE}/advent-calendar/`,
+        `${MY_SITE}/memory-game/`,
+        `${MY_SITE}/encriptador-de-texto/`,
+      ],
+      i18n: {
+        defaultLocale: "es",
+        locales: {
+          es: "es-EC",
+          en: "en-US",
+        },
+      },
+    }),
+  ],
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
